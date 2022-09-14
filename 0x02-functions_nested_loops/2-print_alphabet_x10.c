e1766decb0f98e7d738a27fbc8f7315f5d113bd5
Adetops <heritagediwamu@gmail.com>
2022-09-13 20:49:14 -0700

    Print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that prints 10 times the alphabet in lowercase
+ * followed by a new line
+ */
+void print_alphabet_x10(void);
+{
+	char j;
+	int i;
+
+	while (i = '0'; i <= 10)
+	{
+		while (j = 'a'; j <= 'z')
+		{
+			_putchar(j);
+			j++
+		}
+		_putchar('\n');
+		i++
+	}
+}
