c26b62dffb11258f63ee33a0f83895037495eb19
Adetops <heritagediwamu@gmail.com>
2022-09-14 20:21:47 -0700

    Print a sequence

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * followed by a new line
+ * Numbers must be separated by a comma, followed by a space
+ * @n: the first printed number
+ * The last printed number should be 98
+ */
+void print_to_98(int n)
+{
+	for (n = 0; n < 99; n++)
+	{
+		_putchar('n');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+}
