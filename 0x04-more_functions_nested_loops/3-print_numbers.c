996dec40435bcd17205ddae4dc4ea5cd004c44bf
Adetops <heritagediwamu@gmail.com>
2022-09-17 13:23:57 -0700

    .

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,15 +4,14 @@
  * print_numbers - function that prints the numbers, from 0 to 9
  * followed by a new line
  * You can only use _putchar twice in your code
- * Return: void
  */
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; ++i)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(i);
-		_putchar('\n');
+		_putchar(i + '0');
 	}
+	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,17 +10,16 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; ++1)
+	for (i = 0; i < 10; 1++)
 	{
-		if (i == 2 || i == 4)
+		if (i = 2 || i = 4)
 		{
 			continue;
 		}
 		else
 		{
-			break;
+			_putchar(i + '0');
 		}
-		_putchar(i);
 	}
 	_putchar('\n');
 }
