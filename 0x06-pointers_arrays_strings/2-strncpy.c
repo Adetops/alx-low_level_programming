8cf8b872ea6a7495bbe4666f01a62f51141e0e38
Adetops <heritagediwamu@gmail.com>
2022-09-22 16:11:40 -0700

    PAS 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,13 +5,12 @@
  * @dest: destination
  * @src: source
  * @n: number of bytes of src copied
- *
  * Return: pointer to the string dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	for ( ; i < n; i++)
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - function that reverses the content of an array of integers
+ * @a: an array
+ * @n: number of elements in the array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, arrev;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			arrev = *(a + j);
+			*(a + j) = *(a + j - 1);
+			*(a + (j - 1)) = arrev
+		}
+	}
+}
