1ac2b6fe938a772866d88605eedc0d4c1d9d7094
Adetops <heritagediwamu@gmail.com>
2022-09-22 16:14:01 -0700

    .

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,15 +8,15 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i, j, arrev;
+	int i, j, temp;
 
 	for (i = 0; i < n - 1; i++)
 	{
 		for (j = i + 1; j > 0; j--)
 		{
-			arrev = *(a + j);
+			temp = *(a + j);
 			*(a + j) = *(a + j - 1);
-			*(a + (j - 1)) = arrev
+			*(a + (j - 1)) = temp
 		}
 	}
 }
