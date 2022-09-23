620ff32f44ac1e351299aa2969e162d3ccef413c
Adetops <heritagediwamu@gmail.com>
2022-09-23 16:41:01 -0700

    Advanced correct

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -20,9 +20,9 @@ char *leet(char *s)
 
 	while (*(s + count) != '\0')
 	{
-		for (i = 0; i != '\0'; i++)
+		for (i = 0; i < 10; i++)
 		{
-			if (*(s + count) == letters[i])
+			if (*(s + count) = letters[i])
 				*(s + count) = numbers[i];
 		}
 		count++;
