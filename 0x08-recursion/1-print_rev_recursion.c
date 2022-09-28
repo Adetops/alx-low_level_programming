86ee227a988fada1a4e1bbd2e394984a4ed4780a
Adetops <heritagediwamu@gmail.com>
2022-09-28 15:46:53 -0700

    .

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,11 +7,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar(*s);
-		_print_rev_recursion(s - 1);
-	}
 	if (s == 0)
 		_putchar('\n');
+	if (*s == '\0')
+	{
+		_print_rev_recursion(s - 1);
+	}
 }
