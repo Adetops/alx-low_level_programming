57e7e922b146ad4f849becb171f3a7c4fe9398dd
Adetops <heritagediwamu@gmail.com>
2022-09-28 14:20:55 -0700

    Puts rec

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,13 +7,8 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 1;
-
-	if (*s != '\0')
-	{
-		_putchar(*s);
-		_puts_recursion(*s + i);
-	}
-	else
+	if (*s == '\0')
 		_putchar('\n');
+	else
+	_putchar(*s);
 }
