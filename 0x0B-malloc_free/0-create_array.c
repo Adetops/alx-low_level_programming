83aab0da01666f72b401fb657d6832bd887ee224
Adetops <heritagediwamu@gmail.com>
2022-10-05 17:23:07 -0700

    .

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	ptr = (char *)malloc(sizeof(char) * size);
+	ptr = (char *)malloc(sizeof(c) * size);
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
