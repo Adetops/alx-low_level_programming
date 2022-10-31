752fae74326cc2cdf3798ce28a7188a59619c352
Adetops <heritagediwamu@gmail.com>
2022-10-31 15:53:46 -0700

    typedef understood

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,72 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	x = 0;
+
+	while (s[x] != '\0')
+		x++;
+	return (x);
+}
+
+/**
+ * *_strcpy - copies the string pointed to by src including the terminating
+ * null byte (\0) to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+	while (src[len] != '\0')
+		len++;
+	for (i = 0; i < len; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the new dog
+ * @age: age of the new dog
+ * @owner: owner to initialize for the new dog
+ * Return: struct pointer
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *nd;
+
+	nd = malloc(sizeof(dog_t));
+	if (nd == NULL)
+		return (NULL);
+
+	nd->name = malloc(_strlen(name) + 1);
+	if (nd->name == NULL)
+	{
+		free(nd);
+		return (NULL);
+	}
+	nd->age = age;
+	nd->owner = malloc(_strlen(owner) + 1);
+	if (nd->owner == NULL)
+	{
+		free(nd);
+		free(nd->name);
+		return (NULL);
+	}
+	_strcpy(nd->name, name);
+	_strcpy(nd->owner, owner);
+	return (0);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,26 @@
 #ifndef DOG_H
 #define DOG_H
-/*
-  Define a new type 'struct dog'
- */
-struct dog
+/* Define a new type 'struct dog' */
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog;
 
+/* function that initialize a variable of type struct dog */
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/* function that prints a struct dog */
 void print_dog(struct dog *d);
+
+/* define a new type dog_t as a new name for type struct dog */
+typedef struct dog dog_t;
+
+/* function that creates a new dog */
+dog_t *new_dog(char *name, float age, char *owner);
+
+/* function that frees dogs */
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
