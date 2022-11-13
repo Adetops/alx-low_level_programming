7ec6e31ed9681dc11eb914e4d26feb303d0228a5
Adetops <heritagediwamu@gmail.com>
2022-11-12 19:05:58 -0800

    Print its main opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments passed
+ * @argv: array of argumets
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, arg2;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("%s\n", "Error");
+		exit(1);
+	}
+	arg2 = atoi(argv[1]);
+	if (arg2 < 0)
+	{
+		printf("%s\n", "Error");
+		exit(2);
+	}
+
+	arr = (char *)main;
+	for (i = 0; i < arg2; i++)
+	{
+		if (i == arg2 - 1)
+			printf("%02hhx\n", arr[i]);
+		else if (i < arg2 - 1)
+			printf("%02hhx, ", arr[i]);
+	}
+	return (0);
+}
+
