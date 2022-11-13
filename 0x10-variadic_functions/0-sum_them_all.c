231ddac37b5023fb384f1a58b4aeddbd8d2f2332
Adetops <heritagediwamu@gmail.com>
2022-11-12 21:51:58 -0800

    Tasks 0 and 1

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function that returns the sum of all its parameters
+ * @n: number of parameters
+ * Return: the result of sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, res;
+
+	va_start (ap, n);
+	if (n == 0)
+		return (0);
+	res = 0;
+	for (i = 0; i < n; i++)
+		res += va_arg (ap, int);
+	va_end (ap);
+	return (res);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - function that print numbers, followed by a new line.
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ * Return: always 0
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	va_start (ap, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg (ap, int));
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end (ap);
+}
