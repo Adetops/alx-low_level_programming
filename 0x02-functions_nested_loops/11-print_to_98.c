0ae7c89c9ba312b9dcde9695334c2177a80f0cd1
Adetops <heritagediwamu@gmail.com>
2023-03-04 10:49:14 -0800

    Modifications on Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,29 +7,46 @@
  * Numbers must be separated by a comma, followed by a space
  * @n: the first printed number
  * The last printed number should be 98
+ * Return: nothing.
  */
 void print_to_98(int n)
 {
-	int i, j;
-
-	if (n < 99)
+	if (n != 98)
 	{
-		for (i = n; i <= 98; i++)
+		while (n < 98)
 		{
-			if (i != 98)
-				printf("%d, ", i);
-			else if (i == 98)
-				printf("%d\n", i);
+			printf("%d, ", n);
+			n++;
+		}
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
 		}
 	}
-	else if (n > 97)
-	{
-		for (j = n; j >= 98; j--)
-		{
-			if (j != 98)
-				printf("%d, ", j);
-			else if (j == 98)
-				printf("%d\n", j);
-		}
-	}
+	printf("%d\n", n);
 }
+/**
+ * comment - This is a comment, betty.
+ * int i, j;
+ * if (n < 99)
+ * {
+ * for (i = n; i <= 98; i++)
+ * {
+ * if (i != 98)
+ * printf("%d, ", i);
+ * else if (i == 98)
+ * printf("%d\n", i);
+ * }
+ * }
+ * else if (n > 97)
+ * {
+ * for (j = n; j >= 98; j--)
+ * {
+ * if (j != 98)
+ * printf("%d, ", j);
+ * else if (j == 98)
+ * printf("%d\n", j);
+ * }
+ * }
+ */
