59372c3cba9eeb6ae005a8a7de45198d454d0b34
Adetops <heritagediwamu@gmail.com>
2023-07-07 16:02:56 -0700

    if/else statement modified

--- 0x1A-hash_tables/table_set_3.c
+++ 0x1A-hash_tables/table_set_3.c
@@ -14,32 +14,31 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *dup_value = strdup(value);
 	hash_node_t *new;
 
-	if (1)
-	{
-		index = key_index((unsigned char *)key, ht->size);
-		new = malloc(sizeof(hash_node_t));
+	index = key_index((unsigned char *)key, ht->size);
+	new = malloc(sizeof(hash_node_t));
 	
-		new->key = strdup(key);
-		new->value = dup_value;
-		new->next = NULL;
+	new->key = strdup(key);
+	new->value = dup_value;
+	new->next = NULL;
 
-		if (ht->array != NULL)
+	if (ht->array != NULL)
+	{
+		for (i = 0; i < ht->size; i++)
 		{
-			for (i = 0; i < ht->size; i++)
+			if (i == index)
 			{
-				if (i == index)
+				if (ht->array[i] == NULL)
+					ht->array[i] = new;
+				else
 				{
-					if (ht->array[i] == NULL)
-						ht->array[i] = new;
-					
 					new->next = ht->array[i];
 					ht->array[i] = new;
-    					printf("key: %s\tvalue: %s\n",
-						new->key, new->value);
 				}
-				
+    				printf("key: %s\tvalue: %s at index %lu\n",
+						new->key, new->value, index);
 			}
 		}
+
 		return (1);
 	}
 
